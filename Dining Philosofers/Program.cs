71f2d5ce14bb00c7d2d8e88a780f7f251c4b408c
ko <korzhoff.i@gmail.com>
2019-10-31 16:01:41 +0300

    commit

--- Dining Philosofers/Program.cs
+++ Dining Philosofers/Program.cs
@@ -1,32 +1,31 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp7
+namespace Dining_Philosofers
 {
     public sealed class Filosof
     {
-
         static public void Eat(object firstStic, object secondStic, int FilosofNum, int FirstSticNum, int secondSticNum)
         {
             lock (firstStic)
             {
-                Console.WriteLine("Fil: " + FilosofNum + "; s1: " + FirstSticNum);
+                Console.WriteLine("Filosof " + FilosofNum + "; s1: " + FirstSticNum);
                 lock (secondStic)
                 {
-                    Console.WriteLine("Fil: " + FilosofNum + "; s2: " + secondSticNum);
-                    Console.WriteLine("Fil: " + FilosofNum + "; eating ");
+                    Console.WriteLine("Filosof " + FilosofNum + "; s2: " + secondSticNum);
+                    Console.WriteLine("Filosof " + FilosofNum + "; eating ");
                 }
-                Console.WriteLine("Fil: " + FilosofNum + "; release s2: " + secondSticNum);
+                Console.WriteLine("Filosof " + FilosofNum + "; release s2: " + secondSticNum);
             }
-            Console.WriteLine("Fil: " + FilosofNum + "; release s1: " + FirstSticNum);
+            Console.WriteLine("Filosof " + FilosofNum + "; release s1: " + FirstSticNum);
         }
     }
     class Program
     {
-       
+
         static void Main(string[] args)
         {
             var chopSticks = new Dictionary<int, object>(5);
@@ -36,7 +35,7 @@ namespace ConsoleApp7
                 chopSticks.Add(i, new object());
             }
 
-           Task[] tasks = new Task[5];
+            Task[] tasks = new Task[5];
 
             tasks[0] = new Task(() => Filosof.Eat(chopSticks[0], chopSticks[4], 0, 0, 4));
 
@@ -44,11 +43,11 @@ namespace ConsoleApp7
             for (int i = 1; i < 5; i++)
             {
                 int ix = i;
-                tasks[i] = new Task(() => Filosof.Eat(chopSticks[ix-1], chopSticks[ix], ix , ix, ix+1 ));
+                tasks[i] = new Task(() => Filosof.Eat(chopSticks[ix - 1], chopSticks[ix], ix, ix, ix + 1));
             }
 
 
-            Parallel.ForEach(tasks, t=>t.Start());
+            Parallel.ForEach(tasks, t => t.Start());
             Task.WaitAll(tasks);
 
             Console.ReadKey();
